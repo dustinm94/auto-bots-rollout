f62e8a7734df7d1e260723a03d2327d4d8f84519
dustinm94 <dmcallister9405@gmail.com>
2020-01-12 00:00:08 -0500

    moved to functions

--- Scrapper.py
+++ Scrapper.py
@@ -9,66 +9,70 @@ import sqlalchemy
 import json
 
 
-response = get('https://detroit.craigslist.org/d/cars-trucks/search/cta')
-html_soup = BeautifulSoup(response.text, 'html.parser')
+def get_pages():
+    response = get('https://detroit.craigslist.org/d/cars-trucks/search/cta')
+    html_soup = BeautifulSoup(response.text, 'html.parser')
 
-all_posts = html_soup.find('div', class_='search-legend')
-total = int(all_posts.find('span', class_='totalcount').text)
-print(total)
-
-pages = np.arange(0, total+1, 120)
-
-iterations = 0
-
-post_timing = []
-post_hoods = []
-post_titles = []
-post_links = []
-post_prices = []
-
-for page in pages:
-    response = get('https://detroit.craigslist.org/d/cars-trucks/search/cta' + 's=' + str(page))
-
-    sleep(randint(1, 5))
-
-    if response.status_code != 200:
-        print('Status Code {}'.format(response.status_code))
-
-    page_html = BeautifulSoup(response.text, 'html.parser')
-
-    posts = html_soup.find_all('li', class_='result-row')
-
-    for post in posts:
-
-        post_datetime = post.find('time', class_='result-date')['datetime']
-        post_timing.append(post_datetime)
-
-        if post.find('span', class_='result-hood') is not None:
-            post_hood = post.find('span', class_='result-hood').text
-            post_hoods.append(post_hood)
-        else:
-            post_hoods.append('')
-
-        post_title = post.find('a', class_='result-title hdrlnk')
-        post_titles.append(post_title.text)
-
-        post_link = post_title['href']
-        post_links.append(post_link)
-
-        post_price = post.a.text.strip().replace('$', '')
-
-        post_prices.append(post_price)
-
-        iterations += 1
-        print("Page " + str(iterations) + " scraped successfully!")
-
-    print("\n")
-
-    print("Scrape complete!")
+    all_posts = html_soup.find('div', class_='search-legend')
+    total = int(all_posts.find('span', class_='totalcount').text)
+    total_pages = np.arange(0, total + 1, 120)
+    return total_pages
 
 
-cars = pd.DataFrame({'posted': post_timing, 'location': post_hoods,
-                    'titles': post_titles, 'price': post_prices, 'link': post_links})
+def scrape(pages):
+
+    iterations = 0
+
+    post_timing = []
+    post_hoods = []
+    post_titles = []
+    post_links = []
+    post_prices = []
+
+    for page in pages:
+        response = get('https://detroit.craigslist.org/d/cars-trucks/search/cta' + '?s=' + str(page))
+
+        sleep(randint(1, 5))
+
+        if response.status_code != 200:
+            print('Status Code {}'.format(response.status_code))
+
+        html_soup = BeautifulSoup(response.text, 'html.parser')
+
+        posts = html_soup.find_all('li', class_='result-row')
+
+        for post in posts:
+
+            post_datetime = post.find('time', class_='result-date')['datetime']
+            post_timing.append(post_datetime)
+
+            if post.find('span', class_='result-hood') is not None:
+                post_hood = post.find('span', class_='result-hood').text
+                post_hoods.append(post_hood)
+            else:
+                post_hoods.append('')
+
+            post_title = post.find('a', class_='result-title hdrlnk')
+            post_titles.append(post_title.text)
+
+            post_link = post_title['href']
+            post_links.append(post_link)
+
+            post_price = post.a.text.strip().replace('$', '')
+
+            post_prices.append(post_price)
+
+            iterations += 1
+            print("Page " + str(iterations) + " scraped successfully!")
+
+        print("\n")
+
+        print("Scrape complete!")
+
+    cars_frame = pd.DataFrame({'posted': post_timing, 'location': post_hoods,
+                        'titles': post_titles, 'price': post_prices, 'link': post_links})
+
+    return cars_frame
 
 
 def db_write(cars_dataframe):
@@ -86,6 +90,9 @@ if __name__ == '__main__':
     db_pass = config['db_pass']
     db_url = config['db_url']
     db_name = config['db_name']
+
+    pages = get_pages()
+    cars = scrape(pages)
 
     db_write(cars)
 
